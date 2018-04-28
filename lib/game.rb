f27e1d7203db29419271580b71518443adfa098d
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-04-28 21:46:23 +0100

    added four different scenarios of ending the game

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,41 @@ class Game
     else
       @board[n - 1] = 'O'
     end
+    if winner == 'X'
+      return 'Cross won!'
+    elsif winner == 'O'
+      return 'Naught won!'
+    end
     @turn += 1
-    @board
+    if @turn == 9
+      return 'You losers'
+    else
+      return @board
+    end
+  end
+
+  def winner
+    #checking horizontal
+    if @board[0] == @board[1] && @board[0] == @board[2]
+      return @board[0]
+    elsif @board[3] == @board[4] && @board[3] == @board[5]
+      return @board[3]
+    elsif @board[6] == @board[7] && @board[6] == @board[8]
+      return @board[6]
+    #checking vertical
+    elsif @board[0] == @board[3] && @board[0] == @board[6]
+      return @board[0]
+    elsif @board[1] == @board[4] && @board[1] == @board[7]
+      return @board[1]
+    elsif @board[2] == @board[5] && @board[2] == @board[8]
+      return @board[2]
+    #checking diagon alley
+    elsif @board[0] == @board[4] && @board[0] == @board[8]
+      return @board[0]
+    elsif @board[2] == @board[4] && @board[2] == @board[6]
+      return @board[2]
+    else
+      nil
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,13 +8,43 @@ describe Game do
 
   describe 'tic(n)' do
     it 'replaces numbers with X and O alternately' do
-      expect(game.tic(4)).to eq [1, 2, 3, 'X', 5, 6, 7, 8, 9]
+      expect(game.tic(1)).to eq ['X', 2, 3, 4, 5, 6, 7, 8, 9]
     end
 
     it 'raises an error if the place is already taken' do
       message = 'This place is taken! Choose somewhere else!'
       game.tic(3)
       expect{ game.tic(3) }.to raise_error message
+    end
+
+    it 'ends the game when one player wins vertically' do
+      game.tic(4)
+      game.tic(2)
+      expect(game.tic(7)).to eq 'Cross won!'
+    end
+
+    it 'ends the game when one player wins horizontally' do
+      game = Game.new
+      for n in [1, 4, 2, 5]
+        game.tic(n)
+      end
+      expect(game.tic(3)).to eq 'Cross won!'
+    end
+
+    it 'ends the game when one player wins Diagon Alley' do
+      game = Game.new
+      for n in [1, 4, 5, 2,]
+        game.tic(n)
+      end
+      expect(game.tic(9)).to eq 'Cross won!'
+    end
+
+    it 'has no winner' do
+      game = Game.new
+      for n in [2, 1, 5, 3, 6, 4, 7, 8]
+        game.tic(n)
+      end
+      expect(game.tic(9)).to eq 'You losers'
     end
   end
 end
